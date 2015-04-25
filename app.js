e61dba0f4ab0ab446f2a95b166f5909db8378d57
fual <f20music@narod.ru>
2015-04-25 18:03:52 +0300

    Радужная консоль

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
-/**
- * Created by айя on 25.04.2015.
+/************
+ * Каждая буква в консоле будет рондомного цвета
  */
-console.log(1)
+/* Подключение модуля меняющего цвета консоли*/
+var colors = require('colors');
+
+//Модуль для вывода цветнойк консоли
+var coloredConsole = (function(){
+    //Выбор рандомного числа
+    var getRandomInt = function (min, max)
+    {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+    //разбиению строки на массив и вывод каждого символа с рандомным цветом
+    var ratherColors = function(str){
+        var arr = str.split(''),
+            output = '';
+        var colors = [
+            'rainbow',
+            'grey',
+            'cyan',
+            'grey',
+            'green',
+            'grey',
+            'cyan',
+            'yellow',
+            'blue',
+            'red'
+        ];
+        for (var i= 0, arrLenght = arr.length; i<arrLenght ; i++){
+            output += arr[i][colors[getRandomInt(0,colors.length-1)]];
+        }
+        return output;
+    };
+    return {
+        "go" : function(sent){
+            console.log(ratherColors(sent));
+        }
+    }
+}());
+
+coloredConsole.go("Amphibians are a class of cold-blooded vertebrates,");
+
